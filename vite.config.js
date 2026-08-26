53ad212b9a3e3949f2f1d1434e802386dcb60027
joshhzhang98 <joshhzhang98@gmail.com>
2026-08-26 13:57:48 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
-{
-  "name": "kitchen-draw",
-  "private": true,
-  "version": "0.0.1",
-  "type": "module",
-  "scripts": {
-    "dev": "vite",
-    "build": "vite build",
-    "preview": "vite preview"
-  },
-  "dependencies": {
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0",
-    "lucide-react": "^0.383.0"
-  },
-  "devDependencies": {
-    "@vitejs/plugin-react": "^4.0.0",
-    "vite": "^4.3.9"
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
